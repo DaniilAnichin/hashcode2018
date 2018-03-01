c4323564289480d4a3c0a575ef9db935a096c5f6
Alexey Savenkov <alexey199753@gmail.com>
2018-03-01 21:16:54 +0200

    check ride possibility

--- input.py
+++ input.py
@@ -11,10 +11,15 @@ class Ride:
         self.y = y
         self.s = s
         self.f = f
-
+    def distance(self):
+        return abs(self.x - self.a) + abs(self.y - self.b)
+    def max_time(self):
+        return self.f - self.s
 
 for i in range(N):
      a, b, x, y, s, f = list(map(int, input().split(' ')))
-     rides.append(Ride(a,b,x,y,s,f))
+     ride = Ride(a,b,x,y,s,f)
+     if ride.distance() <= ride.max_time():
+         rides.append(ride)
 
  
